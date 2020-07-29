a42361f8a226d3b3bcf5528245cff69667b82c7e
freeweed <freeweed.m@gmail.com>
2020-07-30 00:11:25 +0700

    edit gitignore add color

--- _site/server.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize')
-const engine = require('./core/noob')
-const controller = require('./core/controller')
-const model = require('./core/model')
-const authen = require('./core/authen')
-const DataTypes = Sequelize.DataTypes
-
-module.exports = { engine, controller, model, authen, DataTypes }
